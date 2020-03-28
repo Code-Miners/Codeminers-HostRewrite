b252b025bfd7cbe90b9dacfd5aa6390f5e91c541
Simon Proctor <simon@simonproctor.com>
2020-03-28 01:16:27 +0000

    First release of code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Codeminers-HostRewrite
-Utility library for managing forwarded host information in a WAF/Proxy environment
+# Codeminers Host Rewrite Module
+This is a simple rewrite module for websites that can't respond to forwarded host headers. For example, you are on a Paas instance fronted by a WAF and you have to respond to multiple domain names.
+
+Ordinarily you get the hostname as the normal hostname of your Paas instance and other domains come as forwarded hosts in the header collection. This module will attempt to find these and overwrite the incoming host value.
+
+Note that the use of this module is extremely specific.
+
+## Configuration
+
+To configure the module, simply add it to the web.config as show below:
+
+````xml
+<add name="HostRewriteModule" type="Core.Modules.Web.HttpModules.HostRewriteModule, Codeminers.Core.Modules.Web.ForwardedHostRewriteModule" />
+````
---
+++ src/ForwardedHostRewriteModule/HostRewriteModule.cs
@@ -0,0 +1,68 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="HostRewriteModule.cs" company="Code Miners Limited">
+//  Copyright (c) 2019 Code Miners Limited
+//   
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU Lesser General Public License for more details.
+//  
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.If not, see<https://www.gnu.org/licenses/>.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Core.Modules.Web.HttpModules
+{
+    using System;
+    using System.Web;
+
+    /// <summary>
+    /// Simple http module for rewriting the forwarded host headers. For when hosted sites can only listen to the host header
+    /// and we need to use a WAF or some form of proxy.
+    /// </summary>
+    public class HostRewriteModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += RewriteHost;
+        }
+
+        private void RewriteHost(object sender, EventArgs e)
+        {
+            HttpApplication context = sender as HttpApplication;
+            if (context == null)
+            {
+                return;
+            }
+
+            // Grab the standard application gateway forwarded host
+            string potentialHost = context.Request.Headers["X-Original-Host"];
+
+            if (string.IsNullOrWhiteSpace(potentialHost))
+            {
+                // Not found, grab the standard forwarded host
+                potentialHost = context.Request.Headers["X-Forwarded-Host"];
+            }
+
+            if (string.IsNullOrWhiteSpace(potentialHost))
+            {
+                // No value - nothing to do
+                return;
+            }
+
+            context.Request.Headers["Host"] = potentialHost;
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
